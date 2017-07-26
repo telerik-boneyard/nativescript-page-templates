a2c4acb7c88cb5a4f5bb9502546ca21ea5a43035
Alexander Djenkov <djenkov@progress.com>
2017-07-26 14:04:25 +0300

    Update Login page

--- login/app/home/home-page.js
+++ login/app/home/home-page.js
@@ -1,43 +1,16 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+const HomeViewModel = require("./home-view-model");
 
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/
-var HomeViewModel = require("./home-view-model");
-
-var homeViewModel = new HomeViewModel();
+const viewModel = new HomeViewModel();
 
 function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    var page = args.object;
+    /* ***********************************************************
+     * Use the "onNavigatingTo" handler to initialize data for the whole tab
+     * navigation layout as a whole.
+     *************************************************************/
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
+    const page = args.object;
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = homeViewModel;
+    page.bindingContext = viewModel;
 }
 
-/*
-Exporting a function in a NativeScript code-behind file makes it accessible
-to the file’s corresponding XML file. In this case, exporting the onNavigatingTo
-function here makes the navigatingTo="onNavigatingTo" binding in this page’s XML
-file work.
-*/
 exports.onNavigatingTo = onNavigatingTo;
