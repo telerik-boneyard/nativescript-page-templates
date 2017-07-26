9ba59c968ffac916ea059bf946328669688b9d21
Alexander Djenkov <djenkov@progress.com>
2017-07-26 15:23:45 +0300

    Create Blank page

---
+++ blank/app/home/home-view-model.js
@@ -0,0 +1,9 @@
+const observableModule = require("data/observable");
+
+function HomeViewModel() {
+    const viewModel = observableModule.fromObject({});
+
+    return viewModel;
+}
+
+module.exports = HomeViewModel;
