54edfe449c55c022ede7c3d46c03331ce55e955c
Alexander Djenkov <djenkov@progress.com>
2017-07-26 14:04:25 +0300

    Update Login page

--- login/app/login/login-page.js
+++ login/app/login/login-page.js
@@ -1,67 +1,37 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+const LoginViewModel = require("./login-view-model");
 
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/
-var frameModule = require("ui/frame");
-var LoginViewModel = require("./login-view-model");
-
-var loginViewModel = new LoginViewModel();
+const viewModel = new LoginViewModel();
 
 function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    var page = args.object;
+    /* ***********************************************************
+     * Use the "onNavigatingTo" handler to initialize data for the whole
+     * login layout as a whole.
+     *************************************************************/
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
+    const page = args.object;
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = loginViewModel;
+    page.bindingContext = viewModel;
 }
 
-function onLoginFacebookButtonTap(loginResponse) {
-    if (loginResponse && !loginResponse.error) {
-        frameModule.topmost().navigate({
-            moduleName: "home/home-page",
-            clearHistory: true
-        });
-    }
+exports.onNavigatingTo = onNavigatingTo;
+
+/* ***********************************************************
+* TODO: Implement Facebook login.
+*************************************************************/
+function onLoginFacebookButtonTap() {
 }
 
-function onSignInButtonTap() {
-    loginViewModel.signIn();
-}
-
+/* ***********************************************************
+* TODO: Implement Google login.
+*************************************************************/
 function onLoginGoogleButtonTap() {
 }
 
-function onForgotPasswordTap() {
+function onSignInButtonTap() {
+    viewModel.signIn();
 }
 
-/*
-Exporting a function in a NativeScript code-behind file makes it accessible
-to the file’s corresponding XML file. In this case, exporting the onNavigatingTo
-function here makes the navigatingTo="onNavigatingTo" binding in this page’s XML
-file work.
-*/
 exports.onNavigatingTo = onNavigatingTo;
 exports.onLoginFacebookButtonTap = onLoginFacebookButtonTap;
-exports.onSignInButtonTap = onSignInButtonTap;
 exports.onLoginGoogleButtonTap = onLoginGoogleButtonTap;
-exports.onForgotPasswordTap = onForgotPasswordTap;
+exports.onSignInButtonTap = onSignInButtonTap;
